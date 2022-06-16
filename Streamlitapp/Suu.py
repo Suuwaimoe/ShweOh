c6353503162a4d31472ad048bb5035280d9b4e96
Suuwaimoe <107231563+Suuwaimoe@users.noreply.github.com>
2022-06-16 22:23:04 +0700

    Create Suu.py

---
+++ Streamlitapp/Suu.py
@@ -0,0 +1,11 @@
+# ShweOh
+My first repository on Github
+import streamlit as st
+st.title('a small program')
+st.subheader('I am Suu Wai Moe')
+st.write('Multiplication table')
+num = int(input('Please enter a number:'))
+for i in range(1,13):
+    result = i * num
+    print(num ,' X ', i , ' => ' , result)
+    
